121cf3781208ac05b24e3e10f7672d0f08a41f8d
gojh91 <gojeda91@naver.com>
2019-09-05 15:47:07 +0900

    no message

---
+++ instascrapy_each/instascrapy_each/items.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class InstascrapyEachItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    each_url = scrapy.Field()
+    each_location = scrapy.Field()
+    address_json = scrapy.Field()
---
+++ instascrapy_each/instascrapy_each/spiders/instagram.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import requests
+import json
+import urllib.request
+import datetime as dt
+import pandas as pd
+import csv
+from instascrapy_each.items import InstascrapyEachItem
+
+class InstagramSpider(scrapy.Spider):
+    name = 'instagram_each'
+    allowed_domains = ['instagram.com']
+    start_urls = []
+    csvname = "키즈존"
+
+    #def start_requests(self):
+    df = pd.read_csv('C:\\Users\\student\\Desktop\\insta_crawling\\인스타 끝난 자료\\' + csvname + '.csv')
+    for each_url in df['each_url']:
+        print("each_url : " + each_url)
+        start_urls.append(each_url)
+           # yield scrapy.Request(url=each_url, callback=self.parse)
+
+    def parse(self, response):
+        print('response.url : ' + response.url)
+        each_json_data = json.loads(response.body)
+        
+        print("each_json_data : " + str(each_json_data))
+        item = InstascrapyEachItem()
+
+        item['each_url'] = response.url
+        
+        item['each_location'] = each_json_data['data']['shortcode_media']['location']['name']
+        
+        item['address_json'] = each_json_data['data']['shortcode_media']['location']['address_json']
+
+        yield item
